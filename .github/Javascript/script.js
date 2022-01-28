3df8767ebe060b73cd08a61bf901cddc42aaf311
abriyusif <abriyusif20@gmail.com>
2022-01-28 09:28:01 +0300

    updated my bio

---
+++ .github/Javascript/script.js
@@ -0,0 +1,38 @@
+//This the let variable. that is variable one
+//let number1;
+//This the var variable. that is variable two
+//let number2;
+//this is constant variable three
+//let number3;
+//
+//variable initialization
+//number1 = 234;
+//number2 = 567;
+//
+//number3 = number1 + number2;
+//
+//console.log(" The result is " + number3);
+
+//let liz = 88;
+//let chuol = 90;
+//let eric = 89;
+//let elie = 88;
+//let shukri = 76;
+//
+//let total = liz + chuol + eric + elie + shukri;
+//
+//let average = total / 5;
+//
+//console.log("The average marks for five students is " + average);
+//
+//function processData(inputString) {
+//console.log(" Hello World");
+//inputString = " Welcome to 30 weeks of code at kakuma!";
+//return inputString;
+//}
+//console.log(processData());
+
+function product(numberone, numbertwo) {
+    return numberone * numbertwo;
+}
+console.log(" The answer is " + product(4, 5));
